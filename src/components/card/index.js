dfc65b75f389d09f9f7649f61ab3c78fefae29f5
Neil Gibson <neilgibson@Neils-MacBook-Air.local>
2021-10-21 22:54:08 +0100

    added some temp json to map card components

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,15 +2,25 @@ import { Image } from "@chakra-ui/image";
 import { Flex, Heading, Text } from "@chakra-ui/layout";
 import React from "react";
 import test from "../../Assets/css/test.css";
+import temp1 from "../../Assets/Images/temp1.png";
 
 function Card() {
   return (
-    <Flex className="hover" flexDir="column" width="30vh" border="1px" p="4px">
-      <Image></Image>
-      <Heading as="h5" size="sm">
+    <Flex
+      className="hover"
+      flexDir="column"
+      h="300"
+      w="40vh"
+      border="1px"
+      p="4px"
+      mt="10px"
+      alignItems="center"
+      borderRadius="10"
+    >
+      <Image src={temp1}></Image>
+      <Heading as="h5" size="sm" mt="25px" color="navy">
         Template 1
       </Heading>
-      <Text>This is the 1st template as it is quite creative</Text>
     </Flex>
   );
 }
--- src/pages/templates/index.js
+++ src/pages/templates/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Flex, Heading } from "@chakra-ui/layout";
 import Navbar from "../../components/navbar";
 import Card from "../../components/card";
+import templates from "../../tempData.json";
 
 function Templates() {
   return (
     <Flex flexDirection="column">
       <Navbar />
       <Flex justifyContent="center" alignItems="center" flexDir="column">
-        <Heading>Choose A Template</Heading>
+        <Heading color="navy">Choose A Template</Heading>
         <Flex mt="10">
           <Card />
         </Flex>
