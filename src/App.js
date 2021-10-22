e389ac5fb9e19972c81c05fc123bdcdb9b2dc131
jeisper <71713717+jeisper@users.noreply.github.com>
2021-10-22 14:55:26 +0100

    Added the routing system

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import Templates from './pages/templates';
+import React from "react";
+import Templates from "./pages/templates";
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Flex } from "@chakra-ui/react";
+import Landing from "./pages/landing";
+import Preview from "./pages/preview";
+import Profile from "./pages/profile";
 
 function App() {
   return (
-    <div className="App">
-
-          <Templates />
-      
-
-    </div>
+    <Router>
+      <Flex>
+        {/* <Navbar /> */}
+        <Switch>
+          <Route path="/templates" component={Templates} />
+          <Route path="/preview" component={Preview} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </Flex>
+    </Router>
   );
 }
 
