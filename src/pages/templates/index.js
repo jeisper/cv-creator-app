3346252e569d480b0c9e442273931505d362328c
Harsh Chandra <harshchandrahc5@gmail.com>
2021-11-26 00:00:18 +0000

    Fixed advanced search

--- src/pages/templates/index.js
+++ src/pages/templates/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Flex, Heading } from "@chakra-ui/layout";
 import Navbar from "../../components/navbar";
 import Card from "../../components/card";
@@ -10,7 +10,7 @@ function Templates() {
   const [search, setSearch] = useState("");
   const [searchResult, setSearchResult] = useState([]);
 
-  let category = null;
+  const category = useRef("all");
 
   //useEffects
   useEffect(() => {
@@ -21,7 +21,6 @@ function Templates() {
 
         // handle success
         setTemplates([...response.data.data.templates]);
-        setTempTemplates([...response.data.data.templates]);
       })
       .catch(function (error) {
         // handle error
@@ -33,45 +32,54 @@ function Templates() {
   const handleCategory = (event) => {
     console.log("this is event: ", event.target.value);
     if (event.target.value === "Creative") {
-      category = "Creative";
+      category.current = "Creative";
     } else if (event.target.value === "Professional") {
-      category = "Professional";
+      category.current = "Professional";
     } else {
-      category = null;
+      category.current = "all";
     }
+
+    showSearchResults();
   };
 
   const handleChange = (event) => {
     setSearch(event.target.value);
+    showSearchResults();
+  };
+
+  const showSearchResults = () => {
     let results = [];
 
+    console.log(category.current);
+    console.log("t", templates);
     for (const template of templates) {
-      if (template.category === category) {
-        if (template.name.toLowerCase().includes(search)) {
+      if (category.current === "all") {
+        console.log("testst", template);
+        if (template.name.toLowerCase().includes(search.toLowerCase())) {
+          // console.log("cat ", template.category);
           results.push(template);
         }
-      }
-      if (category === null) {
-        if (template.name.toLowerCase().includes(search)) {
+      } else {
+        if (
+          template.name.toLowerCase().includes(search) &&
+          template.category
+            .toLowerCase()
+            .includes(category.current.toLowerCase())
+        ) {
           results.push(template);
         }
       }
     }
 
     setSearchResult(results);
-    console.log(searchResult);
+    console.log("here", results);
   };
 
   useEffect(() => {
     if (search === "") {
-      setSearchResult("");
-      setTemplates(tempTemplates);
-    } else if (searchResult.length > 0) {
-      setTemplates(searchResult);
-    } else if (searchResult.length === 0 && search !== "") {
-      setTemplates([]);
+      setSearchResult([]);
     }
-  }, [tempTemplates, search]);
+  }, [search]);
 
   return (
     <Flex flexDirection="column" w="100vw">
@@ -82,19 +90,34 @@ function Templates() {
         search={search}
         setSearch={setSearch}
       />
-      <Flex justifyContent="center" alignItems="center" flexDir="column">
+      <Flex justifyContent="center" alignItems="center" flexDir="column" mt="8">
         <Heading color="navy">Choose A Template</Heading>
-        <Flex
-          w="100%"
-          mt="10"
-          flexWrap="wrap"
-          justifyContent="center"
-          alignItems="center"
-        >
-          {templates.map((item, index) => {
-            return <Card template={item} key={index} />;
-          })}
-        </Flex>
+
+        {search.length > 0 && tempTemplates.length > 0 ? (
+          <Flex
+            w="100%"
+            mt="10"
+            flexWrap="wrap"
+            justifyContent="center"
+            alignItems="center"
+          >
+            {searchResult.map((item, index) => {
+              return <Card template={item} key={index} />;
+            })}
+          </Flex>
+        ) : (
+          <Flex
+            w="100%"
+            mt="10"
+            flexWrap="wrap"
+            justifyContent="center"
+            alignItems="center"
+          >
+            {templates.map((item, index) => {
+              return <Card template={item} key={index} />;
+            })}
+          </Flex>
+        )}
       </Flex>
     </Flex>
   );
